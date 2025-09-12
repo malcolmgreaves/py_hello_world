b57c6e0475a40648d22a7cf82a5d0d74292991b7
Malcolm Greaves <malcolm@eventualcomputing.com>
2025-09-12 12:41:12 -0700

    convert + do it redundantly

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,25 @@
 import sys
 import time
 
+
+def convert(a):
+    try:
+        wait_s = int(a)
+    except:
+        try:
+            wait_s = float(a)
+        except:
+            print(f'UNRECOGNIZED wait_s: "{a}" -- no waiting!')
+            wait_s = None
+    return wait_s
+
+
 def main(wait_s) -> None:
     print("Hello world!")
     
+    if wait_s is not None and not isinstance(wait_s, (float, int)):
+        wait_s = convert(wait_s)
+
     if wait_s is not None and isinstance(wait_s, (float, int)) and wait_s >= 0:
         print(f"Waiting for {wait_s} seconds")
         if wait_s < 1:
@@ -17,15 +33,7 @@ def main(wait_s) -> None:
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
-        a = sys.argv[1]
-        try:
-            wait_s = int(a)
-        except:
-            try:
-                wait_s = float(a)
-            except:
-                print(f'UNRECOGNIZED wait_s: "{a}" -- no waiting!')
-                wait_s = None
+        wait_s = convert(sys.argv[1])
     else:
         wait_s = 60
     main(wait_s)
