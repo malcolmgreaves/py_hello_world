d341e5a5090777ada6e3b9e6e9fe8c3fbe72e689
Malcolm Greaves <malcolm@eventualcomputing.com>
2025-09-11 19:18:24 -0700

    support float and fallback to None on unparsable

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,24 @@
-import os
+import sys
 import time
 
-def main() -> None:
+def main(wait_s = None) -> None:
     print("Hello world!")
     
-    wait_s = int(os.environ.get("WAIT_S", "0"))
-    print(f"[WAIT_S] Waiting for {wait_s} seconds")
-    
-    time.sleep(wait_s)
+    if wait_s is not None and isinstance(wait_s, (float, int)) and wait_s >= 0:
+        print(f"Waiting for {wait_s} seconds")
+        time.sleep(wait_s)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) >= 2:
+        a = sys.argv[1]
+        try:
+            wait_s = int(a)
+        except:
+            try:
+                wait_s = float(a)
+            except:
+                print(f'UNRECOGNIZED wait_s: "{a}" -- no waiting!')
+                wait_s = None
+    else:
+        wait_s = None
+    main(wait_s)
